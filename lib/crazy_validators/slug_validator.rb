cde74b4955f372515f55263d195e8ed2df4fdedb
Cyril Picard <Cyril@picard.ch>
2011-07-07 23:35:57 +0200

    Added a slug validator

---
+++ lib/crazy_validators/slug_validator.rb
@@ -0,0 +1,14 @@
+require 'active_model'
+
+module CrazyValidators
+  class SlugValidator < ActiveModel::EachValidator
+    def validate_each(record, attribute, value)
+      
+      return if value.nil?
+      
+      unless value.to_slug == value
+        record.errors.add(attribute, :not_a_slug, options)
+      end
+    end
+  end
+end

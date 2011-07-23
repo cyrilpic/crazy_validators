141d460811f1a9a4d6dc90a3f8743c197a444193
Cyril Picard <Cyril@picard.ch>
2011-07-23 14:06:22 +0200

    Added french localization

--- lib/crazy_validators.rb
+++ lib/crazy_validators.rb
@@ -6,7 +6,12 @@ module CrazyValidators
   autoload :AlphaNumericalityValidator
   autoload :WordCountValidator
   autoload :BlacklistValidator
+  autoload :SlugValidator
 end
 
 require 'active_support/i18n'
 I18n.load_path += Dir[File.expand_path(File.join(File.dirname(__FILE__), '../locales', '*.yml')).to_s]
+
+ActiveModel::Validations.class_eval do
+  include CrazyValidators
+end

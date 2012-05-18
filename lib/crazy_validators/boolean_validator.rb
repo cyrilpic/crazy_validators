8c0185d5410ae6f857951b0f8714a86a8756c67f
Cyril Picard <Cyril@picard.ch>
2012-05-18 18:56:27 +0200

    fixed boolean validator method

--- lib/crazy_validators/boolean_validator.rb
+++ lib/crazy_validators/boolean_validator.rb
@@ -1,7 +1,7 @@
 module CrazyValidators
   class BooleanValidator < ActiveModel::EachValidator
     def validate_each(record, attribute, value)
-      (value.is_a? TrueClass || value.is_a? FalseClass)
+      value.is_a?(TrueClass) || value.is_a?(FalseClass)
     end    
   end
 end

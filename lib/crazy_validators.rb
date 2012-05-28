c0d5c5c975311593545018528fcc643f33b343bc
Cyril Picard <Cyril@picard.ch>
2012-05-28 21:24:03 +0200

    Changed the way the module is included

--- lib/crazy_validators.rb
+++ lib/crazy_validators.rb
@@ -15,6 +15,4 @@ require 'crazy_validators/word_count_validator'
 require 'active_support/i18n'
 I18n.load_path += Dir[File.expand_path(File.join(File.dirname(__FILE__), '../locales', '*.yml')).to_s]
 
-ActiveModel::Validations.class_eval do
-  include CrazyValidators
-end
+ActiveModel::Validations.__send__(:include, CrazyValidators)
--- test/test_crazy_validators.rb
+++ test/test_crazy_validators.rb
@@ -11,6 +11,11 @@ class TestCrazyValidators < Test::Unit::TestCase
     assert ActiveModel::Validations::constants.include?(:WordCountValidator)
   end
   should "be available in model" do
-    
+    assert SimpleModel.constants.include?(:AlphaNumericalityValidator)
+    assert SimpleModel.constants.include?(:BlacklistValidator)
+    assert SimpleModel.constants.include?(:BooleanValidator)
+    assert SimpleModel.constants.include?(:OEmbedUrlValidator)
+    assert SimpleModel.constants.include?(:SlugValidator)
+    assert SimpleModel.constants.include?(:WordCountValidator)
   end
 end
